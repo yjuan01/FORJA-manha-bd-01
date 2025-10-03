99003731f2e8fa457e3b79aa89105a231b3e06bc
soares <juansoaress714@gmail.com>
2025-10-03 11:47:24 -0300

    Add files via upload

--- front/script.js
+++ front/script.js
@@ -1,3 +1,39 @@
 const resultsDiv = document.getElementById("results")
 
-console.log(resultsDiv)
+
+async function fetchFuncionarios(){
+    resultsDiv.innerHTML = "<p>Carregando...</p>"
+
+    try {
+
+        const response = await fetch('../Dados/data.json')
+        const data = await response.json()
+        console.log(data)
+        
+        if(data.error){
+            resultsDiv.innerHTML = "<p>Erro ao buscar dados!</p>"
+            return
+        }
+        resultsDiv.innerHTML = ''
+
+        data.forEach(item => {
+            const card = document.createElement("div")
+            card.className = "card"
+            card.innerHTML = `
+                <img src="${item.imagem}" alt="${item.nome}"
+                <h2>${item.nome_funcionario}</h2>
+                <h3>${item.cargo}</h3>
+                <h4>${item.descricao}</h4>
+                <p>$${item.salario.toFixed(2)}</p>
+            `
+            resultsDiv.appendChild(card)
+        });
+
+
+    } catch (error) {
+        resultsDiv.innerHTML = "<p>Erro ao buscar produtos!</p>"
+    }
+
+}
+
+fetchFuncionarios()
